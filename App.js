26bf81d0af89d617be6c75edcfd104d9d55ee5f0
Alishow1111 <alishow1111@gmail.com>
2023-11-26 17:18:43 +0000

    API request is working

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch('https://nc-news-2.onrender.com/api/articles')
+      .then((response) => {
+          response.json()
+          .then((json) => {
+            setData(json);
+            console.log(json);
+          })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      })
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <Text>Done!</Text>
+        // <FlatList
+        // data={data}
+        // renderItem={({item}) => <Text style={styles.item}>{item.title}</Text>}/>
+      )}
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 22,
+  },
+  item: {
+   padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
