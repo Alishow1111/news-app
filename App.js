0f049c443128675390dc86e35749a57b7cecd2e9
Alishow1111 <alishow1111@gmail.com>
2023-11-28 21:23:54 +0000

    Fixed positioning of FlatList

--- App.js
+++ App.js
@@ -5,21 +5,19 @@ import {FlatList, StyleSheet, Text, View} from 'react-native';
 export default function App() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-  
+
   useEffect(() => {
-    fetch('https://nc-news-2.onrender.com/api/articles')
+    fetch('https://newsdata.io/api/1/news?apikey=pub_33583e80fdf07a4f0e0b306793eba5782ca2e&q=gaza&language=en')
       .then((response) => {
           response.json()
           .then((json) => {
             setData(json);
             console.log(json);
+            setLoading(false);
           })
       })
       .catch((error) => {
         console.log(error);
-      })
-      .finally(() => {
-        setLoading(false);
       })
   }, [])
   
@@ -28,24 +26,27 @@ export default function App() {
       {loading ? (
         <Text>Loading...</Text>
       ) : (
-        <Text>Done!</Text>
-        // <FlatList
-        // data={data}
-        // renderItem={({item}) => <Text style={styles.item}>{item.title}</Text>}/>
+        <FlatList
+        style={styles.flatList}
+        data={data.results}
+        renderItem={({item}) => <Text style={styles.item}>{item.title}</Text>}/>
       )}
     </View>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 22,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 100
+  },
+  flatList: {
+    width: '80%', // Ensure the FlatList takes the full width
   },
   item: {
-   padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
+    marginBottom: 20,
+  }
 });
+
